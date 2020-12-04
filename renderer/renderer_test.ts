9fd8e6cbb860ef8434881af38984e1b982e9332f
Doug Parker <dgp1130@users.noreply.github.com>
2020-12-04 15:46:47 -0800

    Adds simple end-to-end test of renderer.
    This invokes the renderer as a child process with a test input template and verifies that it outputs the rendered version.

--- renderer/renderer_test.ts
+++ renderer/renderer_test.ts
@@ -1,7 +1,40 @@
 import 'jasmine';
 
+import { execFile as execFileCb } from 'child_process';
+import { promises as fs } from 'fs';
+import * as path from 'path';
+import { promisify } from 'util';
+
+const execFile = promisify(execFileCb);
+
+const testTmpDir = process.env['TEST_TMPDIR']!;
+if (!testTmpDir) fail('$TEST_TMPDIR not set.');
+
+const runfiles = process.env['RUNFILES']!;
+if (!runfiles) fail('$RUNFILES not set.');
+
+const renderer = `${runfiles}/rules_njk/renderer/renderer.sh`;
+async function run(template: string):
+        Promise<{ stdout: string, stderr: string }> {
+    return await execFile(renderer, [ '--template', template ]);
+}
+
 describe('renderer', () => {
-    it('renders', () => {
-        expect(true).toBe(true);
+    let tmpDirPath: string;
+    beforeEach(async () => {
+        tmpDirPath = await fs.mkdtemp(path.join(testTmpDir, 'renderer-'));
+    });
+    afterEach(async () => {
+        await fs.rmdir(tmpDirPath, { recursive: true });
+    });
+
+    it('renders', async () => {
+        await fs.writeFile(`${tmpDirPath}/foo.njk`, `
+            Hello, {{ name }}!
+        `.trim());
+
+        const { stdout } = await run(`${tmpDirPath}/foo.njk`);
+        
+        expect(stdout.trim()).toBe('Hello, World!');
     });
 });
