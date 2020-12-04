7247b2e4489a79318f285f37f0ca33cd6ba78348
Doug Parker <dgp1130@users.noreply.github.com>
2020-12-04 14:42:14 -0800

    Adds `--template` option to choose which template to render.
    I opted to use `yargs` for option handling because I'm a helpless sheeple who can't make my own decisions.
    
    Unfortunately, Nunjucks seems to expect all its templates to be in the same directory, so we can't just give it a file path and expect it to render. Instead, we need to set it up with a particular directory and then load the file by name in that directory. It's really awkward and will likely be a pain in the future.

--- renderer/renderer.ts
+++ renderer/renderer.ts
@@ -1,5 +1,18 @@
 import * as nunjucks from 'nunjucks';
+import * as path from 'path';
+import yargs from 'yargs';
 
-console.log(nunjucks.renderString('Hello, {{ name }}!', {
+const { template } = yargs(process.argv.slice(2))
+    .scriptName('renderer')
+    .option('template', {
+        type: 'string',
+        describe: 'Path to the Nunjucks template file to render',
+    })
+    .demandOption(['template'])
+    .argv;
+
+const { dir: templateDir, base: templateName } = path.parse(template);
+nunjucks.configure(templateDir);
+console.log(nunjucks.render(templateName, {
     name: 'World',
 }));
