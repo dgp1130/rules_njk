11ed370bc5215f3339f93ac221823ee93fe8fb40
Doug Parker <dgp1130@users.noreply.github.com>
2020-12-04 17:33:36 -0800

    Adds stricter flags to Nunjucks renderer and updates error DX.
    With these new flags, most importantly a template will fail if a user references a symbol that is not known to the renderer.
    
    This also updates the error flow to give concise error messages from Nunjucks but still provide full stack traces for internal errors.
    
    An additional test is added to verify the error use case.

--- renderer/renderer.ts
+++ renderer/renderer.ts
@@ -3,6 +3,17 @@ import * as nunjucks from 'nunjucks';
 import * as path from 'path';
 import yargs from 'yargs';
 
+const nunjucksOptions: nunjucks.ConfigureOptions = {
+    autoescape: true,
+    throwOnUndefined: true,
+};
+
+class RenderError extends Error {
+    public constructor(cause: Error) {
+        super(cause.message);
+    }
+}
+
 async function main(): Promise<number> {
     // Process command line arguments.
     const { template, output } = yargs(process.argv.slice(2))
@@ -20,10 +31,15 @@ async function main(): Promise<number> {
 
     // Render the template.
     const { dir: templateDir, base: templateName } = path.parse(template);
-    nunjucks.configure(templateDir);
-    const rendered = nunjucks.render(templateName, {
-        name: 'World',
-    });
+    nunjucks.configure(templateDir, nunjucksOptions);
+    let rendered: string;
+    try {
+        rendered = nunjucks.render(templateName, {
+            name: 'World',
+        });
+    } catch (err) {
+        throw new RenderError(err);
+    }
 
     // Write to the output file location.
     await fs.writeFile(output, rendered);
@@ -31,4 +47,13 @@ async function main(): Promise<number> {
     return 0;
 }
 
-main().then((exitCode) => process.exit(exitCode));
+main().then((exitCode) => process.exit(exitCode), (err) => {
+    if (err instanceof RenderError) {
+        // Render errors are from bad input, show a nice message.
+        console.error(err.message);
+        process.exit(1);
+    } else {
+        // Non-render errors are a tooling failure, give a full stack trace.
+        throw err;
+    }
+});
--- renderer/renderer_test.ts
+++ renderer/renderer_test.ts
@@ -15,11 +15,21 @@ if (!runfiles) fail('$RUNFILES not set.');
 
 const renderer = `${runfiles}/rules_njk/renderer/renderer.sh`;
 async function run(template: string, output: string):
-        Promise<{ stdout: string, stderr: string }> {
-    return await execFile(renderer, [
-        '--template', template,
-        '--output', output,
-    ]);
+        Promise<{ code: number, stdout: string, stderr: string }> {
+    try {
+        const { stdout, stderr } = await execFile(renderer, [
+            '--template', template,
+            '--output', output,
+        ]);
+        return { code: 0, stdout, stderr };
+    } catch (err) {
+        const { code, stdout, stderr } = err as {
+            code: number,
+            stdout: string,
+            stderr: string,
+        };
+        return { code, stdout, stderr };
+    }
 }
 
 describe('renderer', () => {
@@ -31,21 +41,42 @@ describe('renderer', () => {
         await fs.rmdir(tmpDirPath, { recursive: true });
     });
 
-    it('renders', async () => {
+    it('renders successfully', async () => {
         await fs.writeFile(`${tmpDirPath}/foo.njk`, `
             Hello, {{ name }}!
         `.trim());
 
-        const { stdout, stderr } = await run(
+        const { code, stdout, stderr } = await run(
             `${tmpDirPath}/foo.njk`,
             `${tmpDirPath}/foo.html`,
         );
 
+        expect(code).toBe(0);
+
         const generated = await fs.readFile(`${tmpDirPath}/foo.html`, 'utf8');
-        
         expect(generated.trim()).toBe('Hello, World!');
 
         expect(stdout.trim()).toBe('');
         expect(stderr.trim()).toBe('');
+    });
+
+    it('emits an error on render failure', async () => {
+        // User accidentally uses a symbol that does not exist.
+        await fs.writeFile(`${tmpDirPath}/foo.njk`, `
+            Hello, {{ doesNotExist }}!
+        `.trim());
+
+        const { code, stdout, stderr } = await run(
+            `${tmpDirPath}/foo.njk`,
+            `${tmpDirPath}/foo.html`,
+        );
+        
+        expect(code).toBe(1);
+
+        // Output file should not have been created.
+        await expectAsync(fs.access(`${tmpDirPath}/foo.html`)).toBeRejected();
+
+        expect(stdout.trim()).toBe('');
+        expect(stderr.trim()).toContain('attempted to output null or undefined value');
     });
 });
